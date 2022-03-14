d9d6f398a6f8474100b4b39d7f703aa44177ff29
mamalopes <99338751+mamalopes@users.noreply.github.com>
2022-03-14 08:29:01 -0300

    Create codigo.ino

---
+++ Exercícios/ex 6/codigo.ino
@@ -0,0 +1,55 @@
+
+const int botao1 = 6; 
+const int botao2 = 2; 
+const int botao3 = 3; 
+const int botao4 = 4;
+const int botao5 = 5; 
+
+int var1;
+int var2;
+int var3;
+int var4;
+int var5;
+
+
+void setup()
+{
+  pinMode(botao1, INPUT);
+  pinMode(botao2, INPUT);
+  pinMode(botao3, INPUT);
+  pinMode(botao4, INPUT);
+  pinMode(botao5, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  var1 = digitalRead(botao1);
+  var2 = digitalRead(botao2);
+  var3 = digitalRead(botao3);
+  var4 = digitalRead(botao4);
+  var5 = digitalRead(botao5);
+  
+  if(var1){
+  	Serial.println("vamos passear");
+  } 
+  
+   if(var2){
+  	Serial.println("no pinq");
+  } 
+  
+   if(var3){
+  	Serial.println("sextou");
+  } 
+  
+   if(var4){
+  	Serial.println("diversao");
+  } 
+  
+   if(var5){
+  	Serial.println("feliz");
+  } 
+  
+}
+     
